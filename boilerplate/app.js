e5ec03b0cb6bde262bc0e8c8591f1904020dc0b5
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-10-25 12:45:09 -0500

    added swagger docs

--- boilerplate/app.js
+++ boilerplate/app.js
@@ -4,16 +4,18 @@ const express = require('express');
 const employeeRoutes = require('./routes/employee');
 const app = express();
 const port = parseInt(process.env.PORT || '3000');
+const swaggerUi = require('swagger-ui-express'), swaggerDocument = require('./swagger.json');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/employees', employeeRoutes);
 
 // Fail over route
 app.use(function(req, res) {
     res.status(404).send('Not found');
 });
+
 
 // listen for requests
 app.listen(port, function() {
