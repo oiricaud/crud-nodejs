feb103fbb8812a4fac6872814b79f10f4ea4930f
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-10-25 23:12:42 -0500

    enabled cors to allow other microservices access this api

--- boilerplate/app.js
+++ boilerplate/app.js
@@ -1,12 +1,12 @@
 'use strict';
-
+const cors = require('cors');
 const express = require('express');
 const employeeRoutes = require('./routes/employee');
 const app = express();
 const port = parseInt(process.env.PORT || '3000');
 const swaggerUi = require('swagger-ui-express'), swaggerDocument = require('./swagger.json');
 
-app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/employees', employeeRoutes);
