380ffedee38080c94f30589041dc6ccd53353833
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-10-26 10:09:19 -0500

    added express.json

--- boilerplate/app.js
+++ boilerplate/app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = parseInt(process.env.PORT || '3000');
 const swaggerUi = require('swagger-ui-express'), swaggerDocument = require('./swagger.json');
 
+app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -23,3 +24,5 @@ app.listen(port, function () {
 });
 
 module.exports = app;
+
+
